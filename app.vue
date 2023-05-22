9560031ae6b61f95ce5905dbe758d6356853e43a
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2023-05-22 11:19:30 +0300

    new todo

--- app.vue
+++ app.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+const newTodo = ref("");
+
 const { data: todos } = await useFetch("/api/todos");
+
+async function addTodo() {
+  const todo = await $fetch("/api/todos", {
+    method: "POST",
+    body: {
+      title: newTodo.value,
+    },
+  });
+
+  todos.value.push(todo);
+  newTodo.value = "";
+}
 </script>
 
 <template>
+  <div>
+    <input v-model="newTodo" />
+    <button @click="addTodo">Add</button>
+  </div>
+
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.title }}
